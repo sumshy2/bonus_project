6c7bde99dbb623fd1f6b07c570b8efd7761e77ed
Your Name <yentrangdung@wireless-10-147-250-134.public.utexas.edu>
2016-04-28 20:26:17 -0500

    everything that goes with GUI

---
+++ DrawCircle.java
@@ -0,0 +1,28 @@
+package bonus_project;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JPanel;
+
+class DrawCircle extends JPanel{
+	private int x;
+	private int y;
+	private Color c;
+	public DrawCircle(int xa, int ya, Color color)
+	{
+		x = xa;
+		y = ya;
+		c = color;
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		Graphics2D g2d = (Graphics2D)g;
+		Ellipse2D.Double circle = new Ellipse2D.Double(1, 1, x, y);
+		g2d.setColor(c);
+		g2d.fill(circle);
+	}
+}
---
+++ InputText.java
@@ -0,0 +1,18 @@
+package bonus_project;
+
+public class InputText {
+	private String input;
+	
+	public InputText ()
+	{
+		input = new String();
+	}
+	public void setInput(String data)
+	{
+		input = data;
+	}
+	public String getInput()
+	{
+		return input;
+	}
+}
